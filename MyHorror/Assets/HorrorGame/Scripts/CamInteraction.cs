ac0e2c95501aeade6faa435a5d957fa24db5f2b8
LilXenonz <105544636+LilXenonz@users.noreply.github.com>
2025-09-21 23:35:32 +0200

    dialogue system setup text and raycasting

--- MyHorror/Assets/HorrorGame/Scripts/CamInteraction.cs
+++ MyHorror/Assets/HorrorGame/Scripts/CamInteraction.cs
@@ -1,7 +1,15 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CamInteraction : MonoBehaviour
 {
+    public Text InteractionText;
+
+    private float InteractionDis = 5f;
+
+    public bool CanInteraction = true;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,47 @@ public class CamInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (CanInteraction == true)
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, InteractionDis))
+            {
+                if (hit.collider.CompareTag("NPC"))
+                {
+                    InteractionText.text = "Talk To Him";
+
+                    //talk
+
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        CanInteraction = false;
+                        StartCoroutine(TalkToNPC());
+                    }
+                }
+                else
+                {
+                    InteractionText.text = "";
+
+                }
+            }
+
+            else
+            {
+                InteractionText.text = "";
+            }
+        }
+
+    }
+
+    IEnumerator TalkToNPC()
+    {
+
+
+
+        yield return new WaitForSeconds(2f);
     }
 }
+
+
