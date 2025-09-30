53a9521cd89dd11934918fa7ed3b62e2568be9e0
LilXenonz <105544636+LilXenonz@users.noreply.github.com>
2025-09-30 17:55:27 +0200

    car scene dialogue system finished

--- MyHorror/Assets/HorrorGame/Scripts/CarInteraction.cs
+++ MyHorror/Assets/HorrorGame/Scripts/CarInteraction.cs
@@ -1,7 +1,21 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarInteraction : MonoBehaviour
 {
+
+    public Text SubText;
+    private string holder;
+    private float texttypertime = 0.025f;
+
+    private bool CanInteract = true;
+
+    //dialogue
+
+    public GameObject TalkPanel;
+    public Text InteractionText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +26,71 @@ public class CarInteraction : MonoBehaviour
     void Update()
     {
         
+        if(CanInteract == true)
+        {
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 5f))
+            {
+                if (hit.collider.CompareTag("Friend"))
+                {
+                    InteractionText.text = "Talk To Ahmad";
+
+                    if(Input.GetKeyDown(KeyCode.E))
+                    {
+                        StartCoroutine(TalkToAhmad());
+                    }
+                }
+                else
+                {
+                    InteractionText.text = "";
+                }
+            }
+            else
+            {
+                InteractionText.text = "";
+            }
+        }
+
+
+    }
+
+    IEnumerator TalkToAhmad()
+    {
+
+        CanInteract = false;
+
+        InteractionText.text = "";
+
+        TalkPanel.SetActive(true);
+
+        SubText.text = "Me: ";
+        holder = "Are you okey ?";
+
+        foreach (char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(texttypertime);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        SubText.text = "Ahmed: ";
+        holder = "Yeah im fine";
+
+        foreach (char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(texttypertime);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        SubText.text = "";
+        CanInteract = true;
+        TalkPanel.SetActive(false);
+
     }
 }
